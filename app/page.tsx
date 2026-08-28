938898c4e85f6a5715a2bc945d727e218507102c
Korrawit Soodnalao <u6211548@au.edu>
2026-08-28 20:29:12 +0700

    Add LinkedIn icon component, update global styles, and enhance homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <>
-        <h1 className="text-4xl text-white font-extrabold">Hi, I'm <span className="text-primary">Korrawit</span></h1>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-xl font-semibold leading-10 tracking-tight text-accent">
-            Full Stack Developer
-          </h1>
-          <p className="max-w-md text-lg text-white leading-8">
-           I'm a Software Engineer focused on building scalable systems, solving challenging problems, and turning ideas into software that works.
-          </p>
+      <main className="flex flex-1 flex-col justify-center w-full xl:max-w-1/2 items-center justify-between xl:py-32 md:px-16 sm:items-start">
+        <div className="w-full flex flex-wrap flex-row min-h-3/4 p-5">
+          <div className="grow align-center">
+            <h1 className="text-4xl text-white font-extrabold md:text-start text-center">Hi, I'm <span className="text-primary">Korrawit</span></h1>
+            <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+              <h1 className="max-w-xs text-xl font-semibold leading-10 tracking-tight text-secondary">
+                Full Stack Developer
+              </h1>
+              <p className="max-w-md text-lg text-white leading-8">
+                I'm a Software Engineer focused on building scalable systems, solving challenging problems, and turning ideas into software that works.
+              </p>
+            </div>
+          </div>
+          <div className="lg:flex flex-col justify-center self-center hidden">
+            Image here
+          </div>
         </div>
-        </>
-        <>
-        <div></div>
-        </>
       </main>
     </div>
   );
