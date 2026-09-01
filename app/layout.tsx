6fd2c00949fa35b2ad5cd4f3989389311a319880
Korrawit Soodnalao <u6211548@au.edu>
2026-09-01 19:23:55 +0700

    fix: correct CSS class syntax in layout and page components

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           aria-hidden="true"
           className="pointer-events-none fixed inset-0 -z-10
             bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)]
-            bg-[size:40px_40px]
-            [mask-image:radial-gradient(ellipse_at_center,#000_70%,transparent_100%)]
+            bg-size-[40px_40px]
+            mask-[radial-gradient(ellipse_at_center,#000_70%,transparent_100%)]
             [-webkit-mask-image:radial-gradient(ellipse_at_center,#000_70%,transparent_100%)]"
         />
         <Navbar />
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <main className="flex flex-1 flex-col justify-center w-full 2xl:max-w-1/2 items-center justify-between xl:py-32 md:px-16 sm:items-start">
+      <main className="flex flex-1 flex-col justify-center w-full 2xl:max-w-1/2 items-center xl:py-32 md:px-16 sm:items-start">
         <div className="w-full flex flex-wrap flex-row min-h-3/4 p-5">
           <div className="grow content-center">
             <h1 className="text-4xl text-white font-extrabold md:text-start text-center">Hi, I'm <span className="text-primary">Korrawit <span className="md:inline-block hidden">Soodnalao</span></span></h1>
@@ -20,10 +20,10 @@ export default function Home() {
             </div>
             <div className="flex md:justify-start justify-center items-center">
               <Link href="">
-                <LinkedInIcon className="size-[24px] m-2" />
+                <LinkedInIcon className="size-6 m-2" />
               </Link>
               <Link href="">
-                <GithubIcon  className="size-[24px] m-2" />
+                <GithubIcon  className="size-6 m-2" />
               </Link>
             </div>
           </div>
